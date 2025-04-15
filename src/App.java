9fc59dbff3a263cf11c3348420eac7995c65f091
MHFERNANDO <fermarhug@gmail.com>
2025-04-15 07:52:21 -0500

    metodo

--- src/App.java
+++ src/App.java
@@ -4,9 +4,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         MetodoBusqueda busqueda = new MetodoBusqueda();
         int[] arreglo = {1, 2, 3, 4, 5};
-        int[] resultado = busqueda.busquedaLineal(arreglo);
-        for (int i = 0; i < resultado.length; i++) {
-            System.out.println(resultado[i]);
-        }
+        int numeroBuscado = 3;
+        int resultado = busqueda.busquedaLineal(arreglo, numeroBuscado);
+        System.out.println("El resultado de la busqueda es: " + resultado);}
     }
-}
+
--- src/controller/MetodoBusqueda.java
+++ src/controller/MetodoBusqueda.java
@@ -3,12 +3,16 @@ package controller;
 public class MetodoBusqueda {
 
 
-    public int[] busquedaLineal(int[] arreglo){
-        int[] resultado = new int[arreglo.length];
+    public int busquedaLineal(int[] arreglo, int busqueda){
+
         for (int i = 0; i < arreglo.length; i++) {
-            resultado[i] = arreglo[i];
+            if (arreglo[i] == busqueda) {
+                System.out.println("El elemento " + busqueda + " se encuentra en la posicion " + i);
+                int posicion = i;
+                return posicion;
+
         }
-        return resultado;
-    }
+        
+    }return -1;
     
-}
+}}
