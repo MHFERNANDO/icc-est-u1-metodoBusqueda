af2669a54963a39a377189d24d06c2bbc66b68b6
MHFERNANDO <fermarhug@gmail.com>
2025-04-15 07:33:57 -0500

    mayores datos en metodo con la usqueda lineal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import controller.MetodoBusqueda;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MetodoBusqueda busqueda = new MetodoBusqueda();
+        int[] arreglo = {1, 2, 3, 4, 5};
+        int[] resultado = busqueda.busquedaLineal(arreglo);
+        for (int i = 0; i < resultado.length; i++) {
+            System.out.println(resultado[i]);
+        }
     }
 }
--- src/controller/MetodoBusqueda.java
+++ src/controller/MetodoBusqueda.java
@@ -2,5 +2,13 @@ package controller;
 
 public class MetodoBusqueda {
 
+
+    public int[] busquedaLineal(int[] arreglo){
+        int[] resultado = new int[arreglo.length];
+        for (int i = 0; i < arreglo.length; i++) {
+            resultado[i] = arreglo[i];
+        }
+        return resultado;
+    }
     
 }
